927206f696bcf7e0a0093f8a5fc5f222635acadb
Nitko Nitkić <nitko12@hotmail.com>
2023-02-27 18:53:26 +0100

    Added list files

--- src/wrangle_dataset.py
+++ src/wrangle_dataset.py
@@ -1,12 +1,33 @@
 import pandas as pd
 import numpy as np
 import librosa
+import os
 
 path = 'dataset/IRMAS_Training_Data/'
 
 
+def read_data(path_to_root):
+    categories = os.listdir(path_to_root)
+
+    for category in categories:
+        if category == '.DS_Store':
+            continue
+
+        path_to_category = os.path.join(path_to_root, category)
+        files = os.listdir(path_to_category)
+
+        for file in files:
+            if file == '.DS_Store':
+                continue
+
+            path_to_file = os.path.join(path_to_category, file)
+            print(path_to_file)
+
+    # print('Categories: ', categories)
+
+
 def main():
-    pass
+    read_data(path)
 
 
 if __name__ == '__main__':
