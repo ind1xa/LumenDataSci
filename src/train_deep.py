50dc8b7ce84629bb8409fb752e6b5cd1e5c48616
Dominik Babić <60478910+babe1304@users.noreply.github.com>
2023-04-12 14:42:57 +0200

    Sequential model

--- src/train_deep.py
+++ src/train_deep.py
@@ -6,17 +6,20 @@ path = 'dataset/IRMAS_Training_Data'
 
 
 def main():
+    input_shape = (130, 180, 1)
+    num_classes = 10
 
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Conv2D(
-            32, (10, 10), activation='relu', input_shape=(100, 130, 1)),
-        tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
         tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-        tf.keras.layers.MaxPooling2D((2, 2)),
-        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
         tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(64, activation='relu'),
-        tf.keras.layers.Dense(11, activation='softmax')
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(num_classes, activation='softmax')
     ])
 
     gen = read_data(path)
